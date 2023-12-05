fbee75d164679f7361cace94b1b05d6a58f4988a
Bhavanees <152387226+Bhavanees@users.noreply.github.com>
2023-12-05 16:29:42 +0530

    Add files via upload

---
+++ Task 3.py
@@ -0,0 +1,8 @@
+name = "Bhavaneeswaran"
+address = "Salem"
+degree = "Bsc"
+subject_marks = [85, 90, 78, 92, 88]
+total = sum(subject_marks)
+average = total / len(subject_marks)
+
+print(f"Name: {name}\nAddress: {address}\nDegree: {degree}\nSubject Marks: {subject_marks}\nTotal: {total}\nAverage: {average}")
